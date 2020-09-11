e7b4c8e89caea1cbc82109907bd6941e90f2c275
danqiangda <scudanqiangda@gmail.com>
2020-09-11 09:59:54 +0800

    understand how stage works

--- test1.cpp
+++ test1.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include <vector>
-#include <string>
+#include <algorithm>
 using namespace std;
 int main()
 {
-    cout<<"hellokity";
+    std::cout << "Hello world" << std::endl;
     return 0;
 }
 
